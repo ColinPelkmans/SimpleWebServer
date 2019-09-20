109419f6ebfd57800f0c1d0ffb8f198e19099d3a
Colin Pelkmans <colin.pelkmans@gmail.com>
2019-09-20 14:46:18 +0200

    Added express

--- src/app.js
+++ src/app.js
@@ -1,34 +1,56 @@
-const http = require("http");
-const url = require("url");
-const port = 3000;
+// const http = require("http");
+// const url = require("url");
+// const port = 3000;
 
-function set_response(response, content, status = 200) {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write(content);
-}
+// function set_response(response, content, status = 200) {
+//   response.writeHead(200, { "Content-Type": "text/plain" });
+//   response.write(content);
+// }
 
-const server = http.createServer();
+// const server = http.createServer();
 
-/*const set_response = (response, content) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write(content);
-};
-*/
+// /*const set_response = (response, content) => {
+//   response.writeHead(200, { "Content-Type": "text/plain" });
+//   response.write(content);
+// };
+// */
 
-server.on("request", (request, response) => {
-  let path = url.parse(request.url).pathname;
-  console.log(path);
+// server.on("request", (request, response) => {
+//   let path = url.parse(request.url).pathname;
+//   console.log(path);
 
-  if (path === "/") {
-    set_response(response, "Hello world");
-  } else if (path === "/about") {
-    set_response(response, "Created by VIVES peoples");
-  } else {
-    set_response(response, "Error", 400);
-  }
-  response.end();
+//   if (path === "/") {
+//     set_response(response, "Hello world");
+//   } else if (path === "/about") {
+//     set_response(response, "Created by VIVES peoples");
+//   } else {
+//     set_response(response, "Error", 400);
+//   }
+//   response.end();
+// });
+
+// server.listen(port, () => {
+//   console.log("Node server created at port " + port);
+// });
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+//Basic routes
+app.get("/", (request, response) => {
+  response.send("Hello World");
 });
 
-server.listen(port, () => {
-  console.log("Node server created at port " + port);
+app.get("/about", (request, response) => {
+  response.send("This app was made by VIVES peoples");
 });
+
+//Express error handling middleware
+app.use((request, response) => {
+  response.type("text/plain");
+  response.status(505);
+  response.send("Error page");
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
